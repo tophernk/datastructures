c66cdc429522984b334262511246be5e33ce2824
tophernk <topher.nk@googlemail.com>
2019-07-04 22:01:13 +0200

    added operations: first, append

--- list1.c
+++ list1.c
@@ -8,18 +8,30 @@ typedef struct node {
 } node_t;
 
 struct node *rest(node_t *list);
+int first(node_t *list);
+struct node *append(node_t *list, int);
 
 void print(node_t *head) {
-	node_t *current = head;
+	if(head->next == NULL) {
+		return;
+	}
+	node_t *current = head->next;
 
 	while (current != NULL) {
-		printf("%d\n", current->value);
+		printf("%d\n", first(current));
 		current = rest(current);
 	}
 }
 
 void *empty() {
 	return malloc(sizeof(node_t));
+}
+
+int first(node_t *list) {
+	if(list != NULL) {
+		return list->value;
+	}
+	return -1;
 }
 
 struct node *rest(node_t *list) {
@@ -29,14 +41,32 @@ struct node *rest(node_t *list) {
 	return NULL;
 }
 
-int main() {
-	node_t *head = empty();
-	head->value = 1;
-	head->next = malloc(sizeof(node_t));
-	head->next->value = 2;
-	head->next->next = NULL;
+struct node *append(node_t *list, int elem) {
+	node_t *current = list;
 
-	print(head);
+	while(current->next != NULL) {
+		current = rest(current);
+	}
+
+	node_t *next_elem = malloc(sizeof(node_t));
+	next_elem->value = elem;
+	next_elem->next = NULL;
+
+	current->next = next_elem;
+
+	return list;
+}
+
+int main() {
+	node_t *list = empty();
+
+	append(list, 1);
+	append(list, 2);
+	append(list, 3);
+
+	print(list);
+
+	print(empty());
 
 	return 0;
 }
