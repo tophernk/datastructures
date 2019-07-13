c7b8525198f5c0854b53e90366e3390f1519bf36
tophernk <topher.nk@googlemail.com>
2019-07-13 23:06:52 +0200

    implemented operations: empty + maketree

--- tree.c
+++ tree.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 typedef struct tree
 {
@@ -21,9 +22,31 @@ tree_t *right(tree_t*);
 bool is_empty(tree_t*);
 
 void *empty() {
-    malloc(sizeof(tree_t));
+    return malloc(sizeof(tree_t));
+}
+
+tree_t *maketree(tree_t *l,int v, tree_t *r) {
+    tree_t *t = empty();
+    node_t *n = malloc(sizeof(node_t));
+    n->left=l;
+    n->right=r;
+    n->value=v;
+    t->root=n;
+}
+
+void print_tree(tree_t *t) {
+    if(t->root != NULL) {
+        printf("%d\n", t->root->value);
+        print_tree(t->root->left);
+        print_tree(t->root->right);
+    }
 }
 
 int main() {
+    tree_t *l = maketree(empty(), 1, empty());
+    tree_t *r = maketree(empty(), 2, empty());
+    tree_t *t = maketree(l, 0, r);
+    print_tree(t);
+
     return 0;
 }
